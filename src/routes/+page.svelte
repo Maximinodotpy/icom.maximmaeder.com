4cc7f4ffa31e9dc68a43d9b09eefda7bace0b29b
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2026-07-12 09:27:16 +0200

    more changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,22 @@
 	import * as config from '$lib/config'
 
 	let { data } = $props()
+
+	const viewtypes = [
+		{
+			name: 'List',
+			icon: 'https://api.iconify.design/material-symbols:format-list-bulleted.svg',
+		},
+		{
+			name: 'Grid',
+			icon: 'https://api.iconify.design/material-symbols:grid-view.svg',
+		},
+		{
+			name: 'Folders',
+			icon: 'https://api.iconify.design/material-symbols:folder.svg',
+		}
+	]
+	let current_viewtype = $state(viewtypes[0])
 </script>
 
 <svelte:head>
@@ -10,18 +26,83 @@
 </svelte:head>
 
 <section>
-	<ul class="posts">
-		{#each data.posts as post}
-			<li class="post">
-				<a href={post.slug} class="title">{post.title}</a>
-				<p class="date">{formatDate(post.date)}</p>
-				<p class="description">{post.description}</p>
-			</li>
+	<!-- Viewtypes -->
+	<div class="viewtypes-list">
+		{#each viewtypes as viewtype}
+			<button class="viewtype {current_viewtype.name == viewtype.name ? 'active' : ''}" onclick={() => {
+				console.log('active viewtype ...');
+				console.log(viewtype);
+				current_viewtype = viewtype
+			}}>
+				<img src={viewtype.icon} alt={viewtype.name} />
+				{ viewtype.name }
+			</button>
 		{/each}
-	</ul>
+	</div>
+
+	{#if data.posts.length === 0}
+		<p>No posts found.</p>
+	{:else if current_viewtype.name === 'Grid'}
+		<div class="posts grid">
+			{#each data.posts as post}
+				<div class="post">
+					<a href={post.slug} class="title">{post.title}</a>
+					<p class="date">{formatDate(post.date)}</p>
+					<p class="description">{post.description}</p>
+				</div>
+			{/each}
+		</div>
+	{:else if current_viewtype.name === 'Folders'}
+		<div class="posts folders">
+			{#each data.posts as post}
+				<div class="post">
+					<a href={post.slug} class="title">{post.title}</a>
+					<p class="date">{formatDate(post.date)}</p>
+					<p class="description">{post.description}</p>
+				</div>
+			{/each}
+		</div>
+	{:else if current_viewtype.name === 'List'}
+		<ul class="posts">
+			{#each data.posts as post}
+				<li class="post">
+					<a href={post.slug} class="title">{post.title}</a>
+					<p class="date">{formatDate(post.date)}</p>
+					<p class="description">{post.description}</p>
+				</li>
+			{/each}
+		</ul>
+	{/if}
 </section>
 
 <style>
+	.viewtypes-list {
+		display: flex;
+		gap: var(--size-3);
+		margin-block-end: var(--size-7);
+
+		img {
+			/* filter: invert(1); */
+			/* opacity: 0.5; */
+		}
+
+		.viewtype.active {
+			background-color: var(--blue-2);
+		}
+
+		button {
+			color: var(--blue-9);
+			background-color: var(--blue-0);
+			border: 1px solid var(--blue-3);
+			box-shadow: none;
+			text-shadow: none;
+
+			&:hover {
+				background-color: var(--blue-1);
+			}
+		}
+	}
+
 	.posts {
 		display: grid;
 		gap: var(--size-7);
@@ -46,6 +127,10 @@
 			.description {
 				margin-top: var(--size-3);
 			}
+		}
+
+		&.grid {
+			grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 		}
 	}
 </style>
