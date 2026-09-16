6476cec36b1f037004b40f0dcc39abaeb65610a6
Maxim Maeder <85794303+Maximinodotpy@users.noreply.github.com>
2026-09-16 10:53:23 +0200

    yee

--- src/posts/prog1/lektionen/2/binSearch/binSearch.java
+++ src/posts/prog1/lektionen/2/binSearch/binSearch.java
@@ -4,15 +4,17 @@ public class binSearch {
     public static void main(String[] args) {
         Map<Integer, int[]> arrays_to_test = new HashMap<>();
 
-        arrays_to_test.put(1, new int[]{10,11,12});
+        arrays_to_test.put(10, new int[]{10,11,12});
 
         for (Map.Entry<Integer, int[]> mapElement: arrays_to_test.entrySet()) {
             System.out.println(mapElement.getKey());
-            System.out.println(mapElement.getValue());
+            System.out.println(Arrays.toString(mapElement.getValue()));
+
+            System.out.println(doBinSearch(mapElement.getValue(), mapElement.getKey()));
         }
     }
 
-    public static int binSearch(int[] arr, int needle) {
+    public static int doBinSearch(int[] arr, int needle) {
         int i = 0;
         int j = arr.length;
         
@@ -22,6 +24,10 @@ public class binSearch {
             position = (i + j) / 2;
 
             int value_at_position = arr[position];
+
+            System.out.println("Checking ...");
+            System.out.println(position);
+            System.out.println(value_at_position);
 
             if (value_at_position == needle || i == (j-1)) {
                 return position;
